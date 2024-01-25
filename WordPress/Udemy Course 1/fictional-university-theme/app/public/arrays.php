68271d145871d9c637f98428740a35e3d790a313
theospasov <teosknow@gmail.com>
2024-01-26 00:19:06 +0200

    Lesson 9

--- WordPress/Udemy Course 1/fictional-university-theme/app/public/arrays.php
+++ WordPress/Udemy Course 1/fictional-university-theme/app/public/arrays.php
@@ -1,4 +1,22 @@
 <?php 
-    $names = array("Brad", 'Chad', 'Emily')
+    $names = array("Barry", 'Chad', 'Emily');
+    $count = 0;
+
+    while($count < count($names)) {
+        echo "<li>Hi my name is $names[$count]</li>";
+        $count++;
+    }
+
 ?>
-<p>Hi my name is <?php echo $names[1] ?></p>
+
+<p>Hi my name is <?php echo $names[0] ?></p>
+
+
+<li></li>
+
+
+<!-- $count = 1;
+    while($count < 10) {
+        echo "<li>$count</li>";
+        $count++;
+    }; -->
