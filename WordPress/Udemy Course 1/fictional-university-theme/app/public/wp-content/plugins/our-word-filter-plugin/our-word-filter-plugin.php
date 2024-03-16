8b0caee8e3c45934a35cb6468ee2a5a64afa1c67
theospasov <teosknow@gmail.com>
2024-03-16 09:42:03 +0200

    Lessons 128 - 129

--- WordPress/Udemy Course 1/fictional-university-theme/app/public/wp-content/plugins/our-word-filter-plugin/our-word-filter-plugin.php
+++ WordPress/Udemy Course 1/fictional-university-theme/app/public/wp-content/plugins/our-word-filter-plugin/our-word-filter-plugin.php
@@ -11,20 +11,62 @@ if (! defined('ABSPATH')) exit; // L127
 class OurWorFilterPlugin {
     function __construct() {
         add_action( 'admin_menu', array($this, 'ourMenu') );
+        
+        if(get_option( 'plugin_words_filter' )) add_filter('the_content', array($this, 'filterLogic'));
+        
+    }
+
+    function filterLogic($content) {
+        $badWords = explode(',', get_option('plugin_words_filter'));
+        $badWordsTrimmed = array_map('trim', $badWords);
+        return str_replace($badWordsTrimmed, '***', $content);
     }
 
     function ourMenu() {
-        add_menu_page('Words to Filter', 'Word Filter', 'manage_options', 'ourwordfilter', array($this, 'wordFilterPage'), 'dashicons-smiley', 20);
+        $mainPageHook = add_menu_page('Words to Filter', 'Word Filter', 'manage_options', 'ourwordfilter', array($this, 'wordFilterPage'), 'data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMjAiIGhlaWdodD0iMjAiIHZpZXdCb3g9IjAgMCAyMCAyMCIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KPHBhdGggZmlsbC1ydWxlPSJldmVub2RkIiBjbGlwLXJ1bGU9ImV2ZW5vZGQiIGQ9Ik0xMCAyMEMxNS41MjI5IDIwIDIwIDE1LjUyMjkgMjAgMTBDMjAgNC40NzcxNCAxNS41MjI5IDAgMTAgMEM0LjQ3NzE0IDAgMCA0LjQ3NzE0IDAgMTBDMCAxNS41MjI5IDQuNDc3MTQgMjAgMTAgMjBaTTExLjk5IDcuNDQ2NjZMMTAuMDc4MSAxLjU2MjVMOC4xNjYyNiA3LjQ0NjY2SDEuOTc5MjhMNi45ODQ2NSAxMS4wODMzTDUuMDcyNzUgMTYuOTY3NEwxMC4wNzgxIDEzLjMzMDhMMTUuMDgzNSAxNi45Njc0TDEzLjE3MTYgMTEuMDgzM0wxOC4xNzcgNy40NDY2NkgxMS45OVoiIGZpbGw9IiNGRkRGOEQiLz4KPC9zdmc+Cg==', 20);
+        // add_menu_page('Words to Filter', 'Word Filter', 'manage_options', 'ourwordfilter', array($this, 'wordFilterPage'), plugin_dir_url(__FILE__) . 'custom.svg', 20);
         add_submenu_page( 'ourwordfilter', 'Words to Filter', 'Words List', 'manage_options', 'ourwordfilter', array($this, 'wordFilterPage'));
         add_submenu_page( 'ourwordfilter', 'Word Filter Options', 'Options', 'manage_options', 'word-filter-options', array($this, 'optionSubPage'));
+        // Adding Custom CSS to the Word Filter Page
+        add_action("load-{$mainPageHook}", array($this, 'mainPageAssets'));
+    }
+
+    function mainPageAssets() {
+        wp_enqueue_style('filterAdminCSS', plugin_dir_url(__FILE__) . 'styles.css');
     }
 
     function optionSubPage() { ?>
         Hello world from options
     <?php }
 
+    function handleForm() {
+       if(wp_verify_nonce( $_POST['ourNonce'], 'saveFilterWords') && current_user_can('manage_options')) {
+            update_option('plugin_words_filter', sanitize_text_field( $_POST['plugin_words_to_filter'] )); ?>
+            <div class="updated">
+                <p>Your filered words were saved</p>
+            </div>
+        <?php } else { ?>
+            <div class="error">
+                <p>Sorry, you don't have permission to perform this action</p>
+            </div>
+       <?php }
+        
+    }
+
     function wordFilterPage() { ?>
-        Hello world
+        <div class="wrap">
+            <h1>Word Filter</h1>
+            <?php if($_POST['justsubmitted'] == "true") $this->handleForm() ?>
+            <form method="POST">
+                <input type="hidden" name="justsubmitted" value="true">
+                <?php wp_nonce_field( 'saveFilterWords', 'ourNonce' ) ?>
+                <label for="plugin_words_to_filter"><p>Enter a <strong>comma-separated</strong> list of words to filter</p></label>
+                <div class="word-filter__flex-container">
+                    <textarea name="plugin_words_to_filter" id="plugin_words_to_filter" placeholder="bad, awful"><?php echo esc_textarea( get_option( 'plugin_words_filter' ) ) ?></textarea>  
+                </div>
+                <input type="submit" name="submit" id="submit" class="button button-primary" value="Save Changes">
+            </form>
+        </div>
     <?php }
 }
 
