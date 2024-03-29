8560cd26454d121dbc61db5bb78dd7fa13e4b86b
theospasov <teosknow@gmail.com>
2024-03-29 15:02:02 +0200

    Lesson 168

--- WordPress/Udemy Course 1/fictional-university-theme/app/public/wp-content/plugins/new-database-table/inc/GetPets.php
+++ WordPress/Udemy Course 1/fictional-university-theme/app/public/wp-content/plugins/new-database-table/inc/GetPets.php
@@ -19,21 +19,19 @@ class GetPets {
   }
 
   function getArgs() {
-    $temp = array(
-      'favcolor' => sanitize_text_field($_GET['favcolor']),
-      'species' => sanitize_text_field($_GET['species']),
-      'minyear' => sanitize_text_field($_GET['minyear']),
-      'maxyear' => sanitize_text_field($_GET['maxyear']),
-      'minweight' => sanitize_text_field($_GET['minweight']),
-      'maxweight' => sanitize_text_field($_GET['maxweight']),
-      'favhobby' => sanitize_text_field($_GET['favhobby']),
-      'favfood' => sanitize_text_field($_GET['favfood']),
-    );
-
-    return array_filter($temp, function($x) {
-      return $x;
-    });
-
+    $temp = [];
+ 
+    if (isset($_GET['favcolor'])) $temp['favcolor'] = sanitize_text_field($_GET['favcolor']);
+    if (isset($_GET['species'])) $temp['species'] = sanitize_text_field($_GET['species']);
+    if (isset($_GET['minyear'])) $temp['minyear'] = sanitize_text_field($_GET['minyear']);
+    if (isset($_GET['maxyear'])) $temp['maxyear'] = sanitize_text_field($_GET['maxyear']);
+    if (isset($_GET['minweight'])) $temp['minweight'] = sanitize_text_field($_GET['minweight']);
+    if (isset($_GET['maxweight'])) $temp['maxweight'] = sanitize_text_field($_GET['maxweight']);
+    if (isset($_GET['favhobby'])) $temp['favhobby'] = sanitize_text_field($_GET['favhobby']);
+    if (isset($_GET['favfood'])) $temp['favfood'] = sanitize_text_field($_GET['favfood']);
+ 
+    return $temp;
+ 
   }
 
   function createPlaceholders() {
