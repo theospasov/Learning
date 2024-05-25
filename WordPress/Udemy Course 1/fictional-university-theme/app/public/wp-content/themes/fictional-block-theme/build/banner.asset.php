f123af858684a67965f2ed0dfd824f8d8c037391
theospasov <teosknow@gmail.com>
2024-05-25 23:16:22 +0300

    WP Lesson 191 - 193

--- WordPress/Udemy Course 1/fictional-university-theme/app/public/wp-content/themes/fictional-block-theme/build/banner.asset.php
+++ WordPress/Udemy Course 1/fictional-university-theme/app/public/wp-content/themes/fictional-block-theme/build/banner.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element'), 'version' => 'e8ccc03682e5169b51ef90ac2c485537');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element'), 'version' => '10f09d1691360e90abe46bd481329073');
